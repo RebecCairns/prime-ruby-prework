ecf4624272f2a178fce2bc01ab27262c57e625e7
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-30 19:23:13 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 
-def prime2?(num) 
+def prime2?(num) # This without iteration just for practice.  I'm trying to code less #convoluted code so I am practicing.  So far I am failing.
   primeNumArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
    #check aginst arr 
    
